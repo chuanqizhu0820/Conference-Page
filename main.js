d13ca833a1f2aae83cde855783895e0ef88bf49c
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-09-08 13:46:08 +0100

    Adjust the styling and debug

--- main.js
+++ main.js
@@ -15,6 +15,35 @@ function Menu() {
 
 btnmenu.addEventListener("click", Menu);
 
+programInfo = [
+    {
+        icon: "img/icons/podium-with-speaker.png",
+        session: "Lecture",
+        activity: "Listen to speaks form different fields of web development and learn the recent advances in web technology"
+    },
+    {
+        icon: "img/icons/exhibition.png",
+        session: "Exhibition",
+        activity: "Appreciate the history and achievement of web technology in all fields of industry"
+    },
+    {
+        icon: "img/icons/comment-discussion.png",
+        session: "Forum",
+        activity: "Take the time to share your thoughts and opinion with experts for interested topic"
+    },
+    {
+        icon: "img/icons/external-teamwork-business-vitaliy-gorbachev-lineal-vitaly-gorbachev.png",
+        session: "Workshop",
+        activity: "Try creating your own work with your teammates"
+    },
+    {
+        icon: "img/icons/social-network.png",
+        session: "Network",
+        activity: "Get opportunities to network with developers around the world"
+    },
+]
+
+
 const programs = document.querySelector("#program-div");
 
 let programHtml = '';
@@ -147,26 +176,3 @@ function closeSpeakers() {
 }
 
 showLess.addEventListener("click", closeSpeakers);
-
-const programs = document.querySelector("#program-div");
-
-let programHtml = '';
-programInfo.forEach(element => {
-    programHtml += ` 
-    <div class="container session-box col-10 col-md-2">
-    <div class="row align-items-center session-content">
-    <div class="imgdiv col-2 col-md-12">
-    <img src="${element.icon}" alt="${element.session}">
-    </div>
-    <div class="session col-2 col-md-12">
-    <p>${element.session}</p>
-    </div>
-    <div class="activity col-8 col-md-12">
-    <p>${element.activity}</p>
-    </div>
-    </div>
-    </div>
-    `
-});
-
-programs.innerHTML = programHtml;
